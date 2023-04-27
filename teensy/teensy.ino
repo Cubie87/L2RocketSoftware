5a80d08f60d752effc780c19d5fd445241f327df
cubie <>
2023-04-27 21:22:07 +0930

    added comments to teensy
    now that it is not the stress of a T-1d launch

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -1,7 +1,9 @@
+// include all files required 
+// todo: move these to src dir bypassing dependencies
 #include <MPU9250_WE.h>
 #include <Wire.h>
 #define MPU9250_ADDR 0x68
- 
+
 #include <SD.h>
 #include <SPI.h>
 File myFile;
@@ -15,7 +17,7 @@ Adafruit_H3LIS331 lis = Adafruit_H3LIS331();
 #include <Adafruit_LPS2X.h>
 
 
-
+// declare led blink functions
 void ledBlink(int LED);
 void accelError(int LED);
 void baroError(int LED);
@@ -24,7 +26,7 @@ void sdError(int LED);
 
 
 
-// Use LPS25 or LPS22 here
+// declare variables as needed
 Adafruit_LPS22 lps;
 Adafruit_Sensor *lps_temp, *lps_pressure;
 
@@ -37,10 +39,14 @@ File csvLogFile;
 
 MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
 
-void setup() {
+void setup(){
+  // start serial bus (will not halt code if serial is not connected)
   Serial.begin(115200);
+
+  // start I2C bus
   Wire.begin();
-  if(!myMPU9250.init()){
+
+  if(!myMPU9250.init()){ // initialise low G accelerometer
     Serial.println("MPU9250 does not respond");
     accelError(LED);
   }
@@ -52,6 +58,7 @@ void setup() {
 
   Serial.println("Position you MPU9250 flat and don't move it - calibrating...");
   delay(1000);
+  // accelerometer setup and calibration
   myMPU9250.autoOffsets();
   myMPU9250.enableGyrDLPF();
   myMPU9250.setGyrDLPF(MPU9250_DLPF_6); // 20hz sampling limit
@@ -64,36 +71,39 @@ void setup() {
 
 
 
-  if (!lps.begin_I2C()) {
+  if(!lps.begin_I2C()){ // initialise barometer
     Serial.println("Failed to find LPS2X chip");
     baroError(LED);
   }
   lis.setDataRate(LIS331_DATARATE_100_HZ);
 
   
-  if (! lis.begin_I2C()) {   // change this to 0x19 for alternative i2c address
+  if(!lis.begin_I2C()){   // initialise high g accelerometer
     Serial.println("Couldnt start");
     while (1) yield();
   }
   lis.setRange(H3LIS331_RANGE_100_G);   // 100, 200, or 400 G!
 
   
-  if (!SD.begin(BUILTIN_SDCARD)) {
+  if(!SD.begin(BUILTIN_SDCARD)){ // initialise SD card
     Serial.println("ERROR WITH SD CARD");
     sdError(LED);
   }
+  // determine a suitable sd card filename.
+  // the oldest file is 0, the youngest file is the highest number
   int i = 0;
   sprintf(csvFilename, "%d.csv", i);
   while(SD.exists(csvFilename)){
     i++;
     sprintf(csvFilename, "%d.csv", i);
   }
+  // touch file
   csvLogFile = SD.open(csvFilename, FILE_WRITE);
-  csvLogFile.print("    Ground Pressure: ");
-  //csvLogFile.print(pressure);
-  csvLogFile.println("    accelerometer unit in m/s^2");
+  // write some basic file headers to the file.
   csvLogFile.println("Millis, Amb Temp(C), Pressure (hPa), Hx, Hy, Hz, Lx, Ly, Lz, gyrX, gyrY, gyrZ, Tx, Ty, Tz, CPU Temp(C)");
-  csvLogFile.close();
+  csvLogFile.close(); // remember to close the file and flush changes!
+
+  // serial debugging. Possibility to turn this into telemetry in the future
   Serial.println("SD CARD Preamble written.");
   Serial.print("DATALOG FILENAME:");
   Serial.println(csvFilename);
@@ -105,21 +115,28 @@ void setup() {
   delay(200);
 }
 
-void loop() {
-
+void loop(){
+  // blink LED as required. This is non-blocking
   ledBlink(LED);
   
+
+  // read low G accelerometer values for acceleration, gyration, and magnetometer
   xyzFloat gValue = myMPU9250.getGValues();
   xyzFloat gyr = myMPU9250.getGyrValues();
   xyzFloat magValue = myMPU9250.getMagValues();
 
+  // read high G accelerometer values
   sensors_event_t event;
   lis.getEvent(&event);
 
+  // read barometer for temperature and pressure
   sensors_event_t temp;
   sensors_event_t pressure;
   lps.getEvent(&pressure, &temp);
 
+
+
+  // write to SD card
   char dataLine[240] = {0}; //  milli,temp, pres, hx,   hy,   hz    lx,   ly,   lz    gyrX, gyrY, gyrZ, Tx,   Ty,   Tz,  CPU Temp(C)         temp,             pres               hx,                   hy,                   hz,                   lowX             lowY             lowZ             
   sprintf(dataLine,             "%ld, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f X", millis(), temp.temperature, pressure.pressure, event.acceleration.x, event.acceleration.y, event.acceleration.z, gValue.x * -9.81, gValue.y * -9.81, gValue.z * 9.81, gyr.x, gyr.y, gyr.z, magValue.x, magValue.y, magValue.z, InternalTemperature.readTemperatureC());
   Serial.println(dataLine);
